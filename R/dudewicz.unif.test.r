e639440fb2a4dbb59458632f128874a9a922c96e
Vladislav Mosin <vmos1999@mail.ru>
2019-11-29 16:21:15 +0300

    Update dudewicz.unif.test.r

--- R/dudewicz.unif.test.r
+++ R/dudewicz.unif.test.r
@@ -1,28 +1,31 @@
-dudewicz.unif.test <- function(x, nrepl=2000, m=length(x) %/% 2)
-{
-    DNAME <- deparse(substitute(x))
-    
-	n <- length(x)
-	
-	statistic <- function(arr) {
-		arr <- sort(arr)
-		a <- arr[1]
-		b <- arr[n]
-		arr <- append(rep(a, m), append(arr, rep(b, m)))
-    	return(-mean(log(n / 2 / m * (arr[c(1:n) + 2 * m] - arr[c(1:n)]), 2)))
-	}
-    h <- statistic(x)
-
-    l <- 0
-	for (i in 1:nrepl)
-    {
-        z <- runif(n)
-        H <- statistic(z)
-        if (H > h) l = l + 1
+dudewicz.unif.test <- function(x, nrepl=2000, m=length(x) %/% 2) {   
+    statistic <- function(arr) {
+        n <- length(arr)
+        arr <- sort(arr)
+        a <- arr[1]
+        b <- arr[n]
+        arr <- append(rep(a, m), append(arr, rep(b, m)))
+        return(-mean(log(n / 2 / m * (arr[c(1:n) + 2 * m] - arr[c(1:n)]), 2)))
     }
-    p.value <- l / nrepl
-
-    RVAL <- list(statistic = c(H = h), p.value = p.value, method = "Dudewicz-van der Meulen test for uniformity", data.name = DNAME)
+    
+    h <- statistic(x)
+    data.name <- deparse(substitute(x))
+    x.length = length(x) 
+    
+    monte_carlo <- function(statistic.value, statistic.function, point.number, nrepl=nrepl) {
+        successes = 0
+        for (i in 1 : nrepl) {
+            if (statistic.function(runif(point.number)) > statistic.value) {
+                successes <- successes + 1
+            }
+        }
+        
+        return(successes / nrepl) 
+    }
+    
+    p.value <- monte_carlo(h, statistic, x.length, nrepl)
+    RVAL <- list(statistic = c(H = h), p.value = p.value, 
+                 method = "Dudewicz-van der Meulen test for uniformity", data.name = data.name)    
     class(RVAL) <- "htest"
     return(RVAL)
 }
