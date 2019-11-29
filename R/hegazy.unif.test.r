3744c8850647a2a492bb93961bcf9f18a8fc5df5
Vitalii Selishchev <800-99@mail.ru>
2019-11-29 15:53:44 +0300

    Fixed Hegazy.

--- R/hegazy.unif.test.r
+++ R/hegazy.unif.test.r
@@ -1,19 +1,28 @@
+GetStatistic <- function(x, n, p)
+{
+    return(sum((abs(x - c(1 : n) / (n + 1))) ^ p) / n)
+}
+
 hegazy.unif.test <- function(x, nrepl=2000, p=1)
 {
     DNAME <- deparse(substitute(x))
-    l <- 0
+    sum <- 0
     n <- length(x)
     x <- sort(x)
-    t <- sum((abs(x - c(1 : n) / (n + 1))) ^ p) / n
+    t <- GetStatistic(x, n, p)
     for (i in 1 : nrepl)
     {
         z <- runif(n)
         z <- sort(z)
-        T <- sum((abs(z - c(1 : n) / (n + 1))) ^ p) / n
-        if (T > t)l = l + 1
+        T <- GetStatistic(z, n, p)
+        if (T > t)
+        {
+            sum <- sum + 1
+        }
     }
-    p.value <- l / nrepl
-    RVAL <- list(statistic = c(T = t), p.value = p.value, method = "Hegazy-Green test for uniformity", data.name = DNAME)
+    p.value <- sum / nrepl
+    RVAL <- list(statistic = c(T = t), p.value = p.value,
+        method = "Hegazy-Green test for uniformity", data.name = DNAME)
     class(RVAL) <- "htest"
     return(RVAL)
 }
