7e0c8c17c060d940c1df8405a30c973ab46e8774
Vladilav Yepifanov <evn1999@yandex.ru>
2019-11-29 15:16:09 +0300

    -copypaste

--- R/dudewicz.unif.test.r
+++ R/dudewicz.unif.test.r
@@ -6,16 +6,18 @@ dudewicz.unif.test <- function(x, nrepl=2000, m=length(x) %/% 2)
     x <- sort(x)
     a <- x[1]
     b <- x[n]
-    for (i in 1:m) x <- c(a, x, b)
-    h <- -mean(log(n / 2 / m * (x[c(1:n) + 2 * m] - x[c(1:n)]), 2))
+	statistic <- function(arr) {
+		for (i in 1:m) arr <- c(a, arr, b)
+    	return(-mean(log(n / 2 / m * (arr[c(1:n) + 2 * m] - arr[c(1:n)]), 2)))
+	}
+    h <- statistic(x)
 	for (i in 1:nrepl)
     {
         z <- runif(n)
         z <- sort(z)
         a <- z[1]
         b <- z[n]
-        for (i in 1:m) z <- c(a, z, b)
-        H <- -1 / n * sum(log(n / 2 / m * (z[c(1:n) + 2 * m] - z[c(1:n)]), 2))
+        H <- statistic(z)
         if (H > h) l = l + 1
     }
     p.value <- l / nrepl
