00fb655514c8f2deea565523c2f8e881c3c716e3
vvselischev <800-99@mail.ru>
2019-11-29 15:39:39 +0300

    Fix Cuiper.

--- R/kuiper.unif.test.r
+++ R/kuiper.unif.test.r
@@ -1,17 +1,26 @@
+GetStatistic <- function(x, n)
+{
+    return(max(x - c(0 : (n - 1)) / n) + max(c(1 : n) / n - x))
+}
+
 kuiper.unif.test <- function(x, nrepl=2000)
 {
     DNAME <- deparse(substitute(x))
-    l <- 0
+    sum <- 0
     n <- length(x)
-    v <- max(x - c(0 : (n - 1)) / n) + max(c(1 : n) / n - x)
+    v <- GetStatistic(x, n)
     for (i in 1 : nrepl)
     {
         z <- runif(n)
-        V <- max(z - c(0 : (n - 1)) / n) + max(c(1 : n) / n - z)
-        if (V > v)l = l + 1
+        V <- GetStatistic(z, n)
+        if (V > v)
+        {
+            sum <- sum + 1
+        }
     }
-    p.value <- l / nrepl
-    RVAL <- list(statistic = c(V = v), p.value = p.value, method = "Kuiper test for uniformity", data.name = DNAME)
+    p.value <- sum / nrepl
+    RVAL <- list(statistic = c(V = v), p.value = p.value,
+        method = "Kuiper test for uniformity", data.name = DNAME)
     class(RVAL) <- "htest"
     return(RVAL)
 }
