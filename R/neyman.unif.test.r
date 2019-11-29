b053f247bd032d6b89fad43acb58ac6e1d7c548f
vvselischev <800-99@mail.ru>
2019-11-29 14:39:35 +0300

    Idea refactoring.

--- R/neyman.unif.test.r
+++ R/neyman.unif.test.r
@@ -1,34 +1,34 @@
 GetNeyman <- function(polynomials, x, n, k)
 {
-	sum <- 0
-	for (i in 1 : k) 
-	{
-		sum <- sum + (sum(sqrt(2) * polynomial.values(polynomials[i + 1], 2 * x - 1)[[1]]) / sqrt(n)) ^ 2
-	}
-	return(sum)
+    sum <- 0
+    for (i in 1 : k)
+    {
+        sum <- sum + (sum(sqrt(2) * polynomial.values(polynomials[i + 1], 2 * x - 1)[[1]]) / sqrt(n)) ^ 2
+    }
+    return(sum)
 }
 
-neyman.unif.test <- function(x, nrepl=2000,k=5)
+neyman.unif.test <- function(x, nrepl = 2000, k = 5)
 {
-	DNAME <- deparse(substitute(x))
-	n <- length(x)
-	x <- sort(x)
-	polynomials <- legendre.polynomials(k,normalized=TRUE)
-	initNeyman <- GetNeyman(polynomials, x, n, k)
-	
-	sum <- 0
-	for (i in 1 : nrepl)
-	{
-		z <- runif(n)
-		z <- sort(z)
-		currentNeyman <- GetNeyman(polynomials, z, n, k)
-		if (currentNeyman > initNeyman)
-		{
-			sum <- sum + 1
-		}
-	}
-	p.value <- l/nrepl
-	RVAL <- list(statistic=c(N=initNeyman), p.value=p.value, method="Neyman test for uniformity", data.name=DNAME)
-	class(RVAL) <- "htest"
-	return(RVAL)
+    DNAME <- deparse(substitute(x))
+    n <- length(x)
+    x <- sort(x)
+    polynomials <- legendre.polynomials(k, normalized = TRUE)
+    initNeyman <- GetNeyman(polynomials, x, n, k)
+
+    sum <- 0
+    for (i in 1 : nrepl)
+    {
+        z <- runif(n)
+        z <- sort(z)
+        currentNeyman <- GetNeyman(polynomials, z, n, k)
+        if (currentNeyman > initNeyman)
+        {
+            sum <- sum + 1
+        }
+    }
+    p.value <- l / nrepl
+    RVAL <- list(statistic = c(N = initNeyman), p.value = p.value, method = "Neyman test for uniformity", data.name = DNAME)
+    class(RVAL) <- "htest"
+    return(RVAL)
 }
