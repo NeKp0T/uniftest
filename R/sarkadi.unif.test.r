bc6cc552c6845764a3dd4963c83697711e664d6f
vvselischev <800-99@mail.ru>
2019-11-29 14:56:44 +0300

    Fixed sarkadi.

--- R/sarkadi.unif.test.r
+++ R/sarkadi.unif.test.r
@@ -1,7 +1,7 @@
 sarkadi.unif.test <- function(x, nrepl=2000)
 {
     DNAME <- deparse(substitute(x))
-    l <- 0
+    sum <- 0
     n <- length(x)
     x <- sort(x)
     d <- (x - c(1 : n) / (n + 1)) / (c(1 : n) * (n - c(1 : n) + 1))
@@ -11,10 +11,13 @@ sarkadi.unif.test <- function(x, nrepl=2000)
         z <- runif(n)
         z <- sort(z)
         D <- (z - c(1 : n) / (n + 1)) / (c(1 : n) * (n - c(1 : n) + 1))
-        J <- n * n * sum(d ^ 2) - n * (sum(d)) ^ 2
-        if (J > j)l = l + 1
+        J <- n * n * sum(D ^ 2) - n * (sum(D)) ^ 2
+        if (J > j)
+        {
+            sum <- sum + 1
+        }
     }
-    p.value <- l / nrepl
+    p.value <- sum / nrepl
     RVAL <- list(statistic = c(J = j), p.value = p.value, method = "Sarkadi-Kosik test for uniformity", data.name = DNAME)
     class(RVAL) <- "htest"
     return(RVAL)
