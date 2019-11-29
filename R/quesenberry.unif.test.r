ee9ffc3d19fdcf38cee8cea29993b19d140ffa58
vvselischev <800-99@mail.ru>
2019-11-29 15:04:51 +0300

    Fixed quasenberry.

--- R/quesenberry.unif.test.r
+++ R/quesenberry.unif.test.r
@@ -1,19 +1,29 @@
+GetStatistic <- function(x, n)
+{
+    return (sum((x[c(2 : (n + 2))] - x[c(1 : (n + 1))]) ^ 2) +
+        sum((x[c(2 : (n + 1))] - x[c(1 : n)]) * (x[c(3 : (n + 2))] - x[c(2 : (n + 1))])))
+}
+
 quesenberry.unif.test <- function(x, nrepl=2000)
 {
     DNAME <- deparse(substitute(x))
-    l <- 0
+    sum <- 0
     n <- length(x)
     x <- c(0, sort(x), 1)
-    q <- sum((x[c(2 : (n + 2))] - x[c(1 : (n + 1))]) ^ 2) + sum((x[c(2 : (n + 1))] - x[c(1 : n)]) * (x[c(3 : (n + 2))] - x[c(2 : (n + 1))]))
+    q <- GetStatistic(x, n)
     for (i in 1 : nrepl)
     {
         z <- runif(n)
         z <- c(0, sort(z), 1)
-        Q <- sum((z[c(2 : (n + 2))] - z[c(1 : (n + 1))]) ^ 2) + sum((z[c(2 : (n + 1))] - z[c(1 : n)]) * (z[c(3 : (n + 2))] - z[c(2 : (n + 1))]))
-        if (Q > q)l = l + 1
+        Q <- GetStatistic(z, n)
+        if (Q > q)
+        {
+            sum <- sum + 1
+        }
     }
-    p.value <- l / nrepl
-    RVAL <- list(statistic = c(Q = q), p.value = p.value, method = "Greenwood-Quesenberry-Miller test for uniformity", data.name = DNAME)
+    p.value <- sum / nrepl
+    RVAL <- list(statistic = c(Q = q), p.value = p.value,
+        method = "Greenwood-Quesenberry-Miller test for uniformity", data.name = DNAME)
     class(RVAL) <- "htest"
     return(RVAL)
 }
